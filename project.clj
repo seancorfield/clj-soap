ff2eb0abca3b9d24a14e17c77543bc37d2afb19e
Sean Corfield <sean@corfield.org>
2013-04-15 09:16:33 -0700

    Update to Clojure 1.5.1 and core.incubator 0.1.2

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/seancorfield/clj-soap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.incubator "0.1.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.incubator "0.1.2"]
                  [org.apache.axis2/axis2-adb "1.6.2"]
                  [org.apache.axis2/axis2-transport-http "1.6.2"]
                  [org.apache.axis2/axis2-transport-local "1.6.2"]]
